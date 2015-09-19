4ded059bb75dad3c28731fdc5d19b25b1436a281
miniserv <mail@ben-keen.com>
2015-09-19 15:57:39 +0100

    Monster generation progressing.

--- class_definitions.php
+++ class_definitions.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once("name_generator.php");
+
 class CharacterSaveData {
 	public $name		= null;		// str
 	public $class		= null;		// str
@@ -53,4 +55,74 @@ class InputFragment {
 	}
 
 	private 		$callback;
+}
+
+class Monster {
+
+	function __construct($playerLevel) {
+
+		// Randomly generate monster level based on player level.
+		$this->InitLevel($playerLevel);
+
+		$this->InitStats($playerLevel);
+
+		$this->name = NameGenerator::Monster($playerLevel);
+	}
+
+	public		$level;			// int
+	public 		$name;			// str
+	public		$hp;			// int
+	public		$hpMax;			// int
+	public 		$elite; 		// bool
+	public		$attack = 0;	// 0, unless elite.
+
+	private function InitLevel($playerLevel) {
+
+		$chanceInHundred = rand(1, 100);
+		echo "Roll: $chanceInHundred\n";
+
+		// Level +3 - 5%
+		if ( $chanceInHundred > 95 ) {
+			$this->level = $playerLevel + 3;
+		}
+		// Level +2 - 10%
+		else if ( $chanceInHundred > 85 ) {
+			$this->level = $playerLevel + 2;
+		}
+		// Level +1 - 20%
+		else if ( $chanceInHundred > 70 ) {
+			$this->level = $playerLevel + 1;
+		}
+		// Level == - 50%
+		else if ( $chanceInHundred > 20 ) {
+			$this->level = $playerLevel;
+		}
+		// Level -1 - 15%
+		else {
+			$this->level = $playerLevel - 1;
+			// Don't want level 0 monsters :P
+			$this->level = max($this->level, 1);
+		}
+	}
+
+	private function InitStats($playerLevel) {
+
+		// Firstly, determine if it's elite.
+		$chanceInHundred 	= rand(1, 100);
+		$this->elite		= $chanceInHundred > 90;
+
+		// Elite monsters simply hit much harder.
+		if ( $this->elite ) {
+			$this->attack = $this->level;
+		}
+
+		$this->hpMax		= $this->level * 10;
+		$this->hp			= $this->hpMax;
+	}
+}
+
+class Room {
+
+	private 	$id;	// int
+
 }
---
+++ name_generator.php
@@ -0,0 +1,73 @@
+<?php
+
+// Names are "higher level" the further down the file we go.
+// i.e. line 1 monster is a Rat, line 30 monster a Tiny Dragon
+class NameGenerator {
+
+	public static function Monster($level) {
+
+		$adjective 	= NameGenerator::GetSingleLine("data/monster_adjectives.txt");
+
+		// Stay in the same vague area of difficulty as the level we're on.
+		$levelAbove	= 10;
+		$monster	= NameGenerator::GetSingleLine("data/monsters.txt", $level, $level + $levelAbove);
+		$article 	= NameGenerator::PrependArticle($adjective);
+
+		$output = $article . " " . $adjective . " " . $monster;
+
+		return $output;
+	}
+
+	public function Weapon($level) {
+
+	}
+
+	public function Armour($level) {
+
+	}
+
+	public function Attack($level) {
+
+	}
+
+	private static function PrependArticle($input) {
+
+		$vowels 	= array("a", "e", "i", "o", "u");
+
+		$firstChar 	= $input[0];
+		$firstChar	= strtolower($firstChar);
+
+		$article	= in_array($firstChar, $vowels) ? "An" : "A";
+
+		return $article;
+	}
+
+	private static function GetLinesFromFile($filePath) {
+
+		$handle = fopen($filePath, "r");
+		$data	= fread($handle, filesize($filePath));
+		fclose($handle);
+
+		$lines	= explode(PHP_EOL, $data);
+
+		return $lines;
+	}
+
+	private static function GetSingleLine($filePath, $overriddenMin = -1, $overriddenMax = -1) {
+
+		$lines 	= NameGenerator::GetLinesFromFile($filePath);
+
+		$min	= 0;
+		$max	= count($lines) - 1;
+		if ( $overriddenMin > 0 && $overriddenMax > 0 ) {
+			// Use overridden values, but stay in bounds.
+			$min = $overriddenMin;
+			$max = min($overriddenMax, $max);
+		}
+
+		$line	= $lines[rand($min, $max)];
+		$line	= rtrim($line);
+
+		return $line;
+	}
+}
